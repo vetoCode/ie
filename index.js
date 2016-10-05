6a105586de1bc6c5285c054ca4149ae6f98162f0
vetoCode <veto.beats@gmail.com>
2016-10-05 11:45:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
-//(function($) {
+(function($) {
   function handleIE() {
     var browser = window.navigator.userAgent;
-
     if (browser.indexOf('MSIE ') > 0 || browser.indexOf('Trident/') > 0) {
       console.log("I'm using an IE..");
-      $('h1').append("I'm using an IE..");
-      //$('h1').innerHTML = "I'm using an IE..";
-      $('h3').attr("style", "display: none;");
+      $('.ieLabel').attr('display', 'inline');
     } else {
       console.log("I'm not using an IE");
-      $('h1').append("I'm not using an IE..");
-      $('h2').hide();
     }
   }
 
   handleIE();
-//})(jQuery);
+})(jQuery);
